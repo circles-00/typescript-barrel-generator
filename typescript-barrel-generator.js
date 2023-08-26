6aa6468d172cf2afe065c69c3f22c4266d51da8c
Nikola Krezeski <nkrezeski@banzae.dev>
2023-08-26 17:30:21 +0200

    initial commit

---
+++ typescript-barrel-generator.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const { readdirSync, writeFileSync } = require('fs')
+
+const args = process.argv.slice(2)
+
+if (args.length === 0) {
+  throw new Error('Please provide a path to a directory')
+}
+
+const [currentDirectoryPath] = args
+const files = readdirSync(currentDirectoryPath)
+
+console.log(files)
+
+let barrelFileContent = ``
+
+files.forEach((file) => {
+  const [fileName, fileExtension] = file.split('.')
+
+  if (!fileExtension || fileExtension === 'ts' || fileExtension === 'tsx') {
+    const fileContent = `export * from './${fileName}'`
+    barrelFileContent += `${fileContent}\n`
+  }
+
+  writeFileSync(`${currentDirectoryPath}/index.ts`, barrelFileContent)
+})
